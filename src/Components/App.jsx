1f918c7ad51641d67337e057ef74f7475e2d7276
ISAI's personal MAC <macover1507@hotmail.com>
2025-04-03 15:48:28 -0600

    first commit

---
+++ src/Components/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function App () {
+  return (
+    <>
+      <main>
+        <header>
+          <h1>Hello World!</h1>
+        </header>
+      </main>
+    </>
+  )
+}
+
+export default App
