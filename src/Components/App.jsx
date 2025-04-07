1593ac425d009327201662d8376053cc99336644
ISAI's personal MAC <macover1507@hotmail.com>
2025-04-07 17:02:51 -0600

    Adding styles to components

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,15 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Products } from './Products';
+import './CSS/App.css'
 
-function App () {
-  return (
-    <>
-      <main>
-        <header>
-          <h1>Hello World!</h1>
-        </header>
-      </main>
-    </>
-  )
+const PRODUCTS_URL = 'https://dummyjson.com/products';
+
+function App() {
+
+    const [listOfProducts, setListOfProducts] = useState([]);
+
+    useEffect(() => {
+
+        fetch(PRODUCTS_URL)
+            .then(res => res.json())
+            .then(response => setListOfProducts(response.products));
+    }, []);
+
+    return (
+        <main>
+            <header>
+                <h1>React Ecommerce App</h1>
+            </header>
+
+            <Products
+                listOfProducts={listOfProducts}
+            />
+        </main>
+    )
 }
 
 export default App
---
+++ src/Components/Products.jsx
@@ -0,0 +1,30 @@
+import React from "react"
+import './CSS/Products.css';
+
+export function Products({ listOfProducts }) {
+
+
+    return (
+        <>
+            <section>
+                <h2>List of products</h2>
+                <div className="products__container">
+                    {
+                        listOfProducts.map(product => {
+                            return (
+                                <div className="product__card" key={product.id}>
+                                    <span className="product__title">{product.title}</span>
+                                    <p className="product__descriptiop">{product.descriptiop}</p>
+                                    <p className="product__brand">{product.brand}</p>
+                                    <p className="product__price">${product.price}</p>
+                                    <img className="product__images" src={product.images[0]} />
+
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+            </section>
+        </>
+    )
+}
