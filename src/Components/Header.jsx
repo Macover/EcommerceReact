de3d32a99b744066d329994efd7feba0a16a2731
Isai PEP <macover1507@hotmail.com>
2025-05-26 15:41:13 -0600

    Modifying the header to 'Tiendita'

---
+++ src/Components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './CSS/Header.css'
+
+export default function Header() {
+
+
+    return (
+        <header className="app__header">
+            <h1 className="header__tile">Tiendita</h1>            
+        </header>
+    )
+
+
+}
