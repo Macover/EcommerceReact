c9cdc9be9f9468ce3b4510216f2e2e10b9c4d484
Isai PEP <macover1507@hotmail.com>
2025-05-26 14:19:02 -0600

    Refactoring app

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 import { Products } from './Products';
 import useFilters from '../Logic/useFilters';
-import './CSS/App.css'
+import './CSS/App.css';
+import Header from './Header';
+import Cart from './Cart';
 
 function App() {
 
-    const { loading, filteredProducts, setFilter } = useFilters();
+    const { loading, filteredProducts } = useFilters();
 
     return (
-        <main>
-            <header>
-                <h1>React Ecommerce App</h1>
-            </header>
-
-            <Products                
+        <main className='main'>
+            <Cart/>
+            <Header />
+            <Products
                 listOfProducts={loading ? [] : filteredProducts}
             />
         </main>
